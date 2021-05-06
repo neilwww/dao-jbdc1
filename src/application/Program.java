a40cb60bd353d31594dde3b01f1613b677ca1988
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-06 02:26:50 -0300

    Interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1," Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmai.com", new Date(), 3000.0, obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
     }
 }
