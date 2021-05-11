05b2a8818ff03cdef1e2ce94a5b733ce17d36242
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-10 21:51:02 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,5 +27,9 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+        System.out.println("=== test4: seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
