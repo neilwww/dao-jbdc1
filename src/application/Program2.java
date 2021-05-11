e4ab7ff489dd21f34132b87ad8211dfe1fc49fcb
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-11 12:40:32 -0300

    final impl depDao and program 2

---
+++ src/application/Program2.java
@@ -0,0 +1,46 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDaoJBDC();
+
+        System.out.println("=== test: department findById ====");
+        Department dep = departmentDao.findById(1);
+        System.out.println(dep);
+
+        System.out.println("=== test2: department findAll ====");
+        List<Department> list = departmentDao.findAll();
+        for (Department obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== test3: department insert ====");
+        Department newDepartment = new Department(5, "Games");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("\n=== test4: department update ====");
+        Department dep2 = departmentDao.findById(1);
+        dep2.setName("comidas");
+        departmentDao.update(dep2);
+        System.out.println("updated!");
+
+        System.out.println("\n=== test: department delete ====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
+    }
+}
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 import model.entities.Seller;
 
 public class DaoFactory {
 
-    public static SellerDao createSellerDao(){
+    public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDaoJDBC createDepartmentDaoJBDC() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
