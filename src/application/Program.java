10e422ebaf68e2ea47c60730ee3cf576a31d504a
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-11 11:12:06 -0300

    seller implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,9 +27,14 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
-        System.out.println("=== test4: seller insert ====");
+        System.out.println("\n=== test4: seller insert ====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("\n=== test5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Renato Cariani");
+        sellerDao.update(seller);
+
     }
 }
